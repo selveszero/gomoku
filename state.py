a05355e9b6a9a7687cd9e9ed7feb3cd3794d2c57
selveszero <49930310+selveszero@users.noreply.github.com>
2021-04-25 12:28:38 -0400

    Update state.py

--- state.py
+++ state.py
@@ -14,49 +14,49 @@ class State(object):
         oponent_count = []
 
         for i in range(game.chain):
-        # if oponent_chain_count == game.chain:
-        #     return int(-10000)
-        # elif current_chain_count == game.chain:
-        #     return int(1000)
-        # elif current_chain_count >= oponent_chain_count:
-        #     return int(current_chain_count)
-        # else:
-        #     return int(-oponent_chain_count)
+            if oponent_chain_count == game.chain:
+                return int(-10000)
+            elif current_chain_count == game.chain:
+                return int(1000)
+            elif current_chain_count >= oponent_chain_count:
+                return int(current_chain_count)
+            else:
+                return int(-oponent_chain_count)
 
-    # def counter(self, player, row, col, dir_x, dir_y):
-    #     count = 0
+    def counter(self, player, row, col, dir_x, dir_y):
+        count = 0
 
-    #     r = row + dir_x
-    #     c = col + dir_y
+        r = row + dir_x
+        c = col + dir_y
 
-    #     while r >= 0 and r < self.board.dimension and c >= 0 and\
-    #             c < self.board.dimension:
-    #         if self.board.board[(r, c)] == player.piece:
-    #             count += 1
-    #             r += dir_x
-    #             c += dir_y
-    #         else:
-    #             break
+        while r >= 0 and r < self.board.dimension and c >= 0 and\
+                c < self.board.dimension:
+            if self.board.board[(r, c)] == player.piece:
+                count += 1
+                r += dir_x
+                c += dir_y
+            else:
+                break
 
-    #     return count
+        return count
 
     def countChains(self, player):
         count = 0
 
         for row in range(self.board.dimension - 1):
             for col in range(self.board.dimension - 1):
-                # vertical_down = self.counter(player, row, col, 1, 0)
-                # vertical_up = self.counter(player, row, col, -1, 0)
-                # horizontal_right = self.counter(player, row, col, 0, 1)
-                # horizontal_left = self.counter(player, row, col, 0, 1)
-                # diagonal_1 = self.counter(player, row, col, 1, 1)
-                # diagonal_2 = self.counter(player, row, col, -1, 1)
-                # diagonal_3 = self.counter(player, row, col, 1, -1)
-                # diagonal_4 = self.counter(player, row, col, -1, -1)
-                # max_chain = max(max_chain, vertical_up, vertical_down,
-                #                 horizontal_right, horizontal_left,
-                #                 diagonal_1, diagonal_2,
-                #                 diagonal_3, diagonal_4)
+                vertical_down = self.counter(player, row, col, 1, 0)
+                vertical_up = self.counter(player, row, col, -1, 0)
+                horizontal_right = self.counter(player, row, col, 0, 1)
+                horizontal_left = self.counter(player, row, col, 0, 1)
+                diagonal_1 = self.counter(player, row, col, 1, 1)
+                diagonal_2 = self.counter(player, row, col, -1, 1)
+                diagonal_3 = self.counter(player, row, col, 1, -1)
+                diagonal_4 = self.counter(player, row, col, -1, -1)
+                max_chain = max(max_chain, vertical_up, vertical_down,
+                                horizontal_right, horizontal_left,
+                                diagonal_1, diagonal_2,
+                                diagonal_3, diagonal_4)
 
         return max_chain
 
